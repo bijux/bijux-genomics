65b0ccac1bc7299e898f0add1fc6ed42d083da0a
bijan <mousavi.bijan@gmail.com>
2026-02-08 20:19:09 +0100

    test(stage-contract): remove duplicate mods

--- crates/bijux-stage-contract/tests/contracts.rs
+++ crates/bijux-stage-contract/tests/contracts.rs
@@ -1,7 +1,3 @@
 // Auto-generated test aggregator for contracts
 #[path = "contracts/versioning.rs"]
 mod mod_contracts_versioning_rs;
-#[path = "contracts/versioning/ssot.rs"]
-mod mod_contracts_versioning_ssot_rs;
-#[path = "contracts/versioning/versioning.rs"]
-mod mod_contracts_versioning_versioning_rs;
