694a8137cd1c79882523a08b6f86cefb80ac3ebb
bijan <mousavi.bijan@gmail.com>
2026-04-01 17:47:36 +0200

    refactor(pipelines): separate fastq default assembly from the defaults root

---
+++ crates/bijux-dna-pipelines/src/fastq/defaults/defaults_assembly.rs
@@ -0,0 +1,16 @@
+use crate::EffectiveDefaults;
+
+use super::parameter_defaults::fastq_default_params;
+use super::rationales::fastq_default_rationales;
+use super::tool_defaults::fastq_default_tools;
+
+pub(super) fn fastq_defaults(paired: bool) -> EffectiveDefaults {
+    let tools = fastq_default_tools();
+    let params = fastq_default_params(paired);
+    let rationales = fastq_default_rationales(&params);
+    EffectiveDefaults {
+        tools,
+        params,
+        rationales,
+    }
+}
