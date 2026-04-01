9a529230c09e3c547f28064901ea97a9005d7d37
bijan <mousavi.bijan@gmail.com>
2026-04-01 13:03:07 +0200

    refactor(pipelines): separate fastq profile identifiers from profile lookup

--- crates/bijux-dna-pipelines/src/fastq/profiles/mod.rs
+++ crates/bijux-dna-pipelines/src/fastq/profiles/mod.rs
@@ -3,12 +3,14 @@
 mod adna_profile;
 mod default_profile;
 mod minimal_profile;
+mod profile_by_id;
 mod profile_contracts;
-mod profile_lookup;
+mod profile_ids;
 mod reference_adna_profile;
 
 pub use adna_profile::fastq_adna_profile;
 pub use default_profile::fastq_default_profile;
 pub use minimal_profile::fastq_minimal_profile;
-pub use profile_lookup::{fastq_profiles_by_id, FASTQ_PROFILE_IDS};
+pub use profile_by_id::fastq_profiles_by_id;
+pub use profile_ids::FASTQ_PROFILE_IDS;
 pub use reference_adna_profile::fastq_reference_adna_profile;
