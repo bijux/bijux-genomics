98ee1679f4aee8e4af94e70de80f10961355174a
bijan <mousavi.bijan@gmail.com>
2026-04-01 14:07:31 +0200

    refactor(core): separate the stable prelude surface from the prelude root

---
+++ crates/bijux-dna-core/src/prelude/stable_surface.rs
@@ -0,0 +1,5 @@
+pub use super::catalog_surface::*;
+pub use super::contract_surface::*;
+pub use super::foundation_surface::*;
+pub use super::identity_surface::*;
+pub use super::metric_surface::*;
