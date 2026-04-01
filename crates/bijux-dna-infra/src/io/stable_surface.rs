d914d247f4e084ef0731831986e55af9b912ca5e
bijan <mousavi.bijan@gmail.com>
2026-04-01 18:36:04 +0200

    refactor(infra): separate the stable io surface from the io root

---
+++ crates/bijux-dna-infra/src/io/stable_surface.rs
@@ -0,0 +1,7 @@
+pub use super::error::{classify_io_error, IoError, IoErrorKind};
+pub use super::read::read_to_end_bounded;
+pub use super::remove::{remove_dir_all, remove_file, remove_file_if_exists, remove_path_if_exists};
+pub use super::write::{
+    atomic_write_bytes, atomic_write_json, atomic_write_with, create_file, ensure_dir, rename,
+    write_bytes, write_string,
+};
