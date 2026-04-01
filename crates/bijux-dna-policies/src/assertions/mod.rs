14b155664e1464dae78635ca913fad3fca9b6419
bijan <mousavi.bijan@gmail.com>
2026-04-01 18:46:01 +0200

    refactor(policies): separate panic assertions from the assertion root

--- crates/bijux-dna-policies/src/assertions/mod.rs
+++ crates/bijux-dna-policies/src/assertions/mod.rs
@@ -1,9 +1,3 @@
 mod comparison_assertions;
 mod condition_assertions;
-
-#[macro_export]
-macro_rules! policy_panic {
-    ($($arg:tt)+) => {
-        panic!("{}", $crate::policy_diagnostics::message(format!($($arg)+)))
-    };
-}
+mod panic_assertion;
