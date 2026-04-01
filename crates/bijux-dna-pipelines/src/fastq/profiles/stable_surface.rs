0dc225780ba42a403721373054b0dcfca89d34d6
bijan <mousavi.bijan@gmail.com>
2026-04-01 17:45:57 +0200

    refactor(pipelines): separate the fastq profile surface from the profile root

---
+++ crates/bijux-dna-pipelines/src/fastq/profiles/stable_surface.rs
@@ -0,0 +1,6 @@
+pub use super::adna_profile::fastq_adna_profile;
+pub use super::default_profile::fastq_default_profile;
+pub use super::minimal_profile::fastq_minimal_profile;
+pub use super::profile_by_id::fastq_profiles_by_id;
+pub use super::profile_ids::FASTQ_PROFILE_IDS;
+pub use super::reference_adna_profile::fastq_reference_adna_profile;
