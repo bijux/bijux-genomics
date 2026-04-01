8c38a6481f920f6345c67546c2135e73667ed895
bijan <mousavi.bijan@gmail.com>
2026-04-01 17:44:52 +0200

    refactor(pipelines): separate the contract surface from the contract root

---
+++ crates/bijux-dna-pipelines/src/contract/stable_surface.rs
@@ -0,0 +1,8 @@
+pub use super::effective_defaults::EffectiveDefaults;
+pub use super::invariants::{
+    InvariantSeverity, InvariantViolationV1, InvariantsPreset, InvariantsReportV1,
+};
+pub use super::pipeline_capabilities::{PipelineCapabilities, PipelineContract};
+pub use super::profile::PipelineProfile;
+pub use super::profile_manifest::ProfileManifestV1;
+pub use super::vocabulary::{ArtifactType, Domain, MetricsBundle, ReportSection, StabilityTier};
