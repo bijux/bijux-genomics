21a562d49172325814382426e7112edcfaa1abad
bijan <mousavi.bijan@gmail.com>
2026-04-01 17:45:14 +0200

    refactor(pipelines): separate the defaults surface from the defaults root

---
+++ crates/bijux-dna-pipelines/src/defaults/stable_surface.rs
@@ -0,0 +1,4 @@
+pub use super::default_params::DefaultParams;
+pub use super::empty_params::EmptyParams;
+pub use super::ledger::{DefaultProvenanceV1, DefaultsLedgerV1};
+pub use super::merge::merge_effective_defaults;
