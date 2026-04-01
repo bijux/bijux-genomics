ef96cc1b53a29ee2c0b62d5fbb77ac1642121cae
bijan <mousavi.bijan@gmail.com>
2026-04-01 18:35:27 +0200

    refactor(infra): separate the stable infra surface from the crate root

---
+++ crates/bijux-dna-infra/src/stable_surface.rs
@@ -0,0 +1,19 @@
+pub use super::hashing::hash_file_sha256;
+pub use super::io::{
+    atomic_write_bytes, atomic_write_bytes_with_retry, atomic_write_json, atomic_write_with,
+    classify_io_error, create_file, ensure_dir, read_to_end_bounded, remove_dir_all, remove_file,
+    remove_file_if_exists, remove_path_if_exists, rename, write_bytes, write_string, IoError,
+    IoErrorKind,
+};
+pub use super::locking::FileLock;
+pub use super::logging::init_logging;
+pub use super::paths::{
+    bench_base_dir, bench_data_dir, bench_suites_dir, bench_tools_dir, configs_dir, configs_file,
+};
+pub use super::retry::{backoff_delay, retry_with, Clock, RetryPolicy, SystemClock};
+pub use super::run_directories::{
+    lock_run, normalize_run_base_dir, pipeline_run_dir, publish_run, run_layout_paths,
+    run_stage_dir, RunLayoutContract, RunLayoutPaths, PIPELINE_RUN_DIR_TEMPLATE,
+    RUN_LAYOUT_CONTRACT,
+};
+pub use super::temp::{temp_dir, temp_dir_in};
