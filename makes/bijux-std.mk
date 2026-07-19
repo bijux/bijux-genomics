3f3ec91f7d8fca2bf2f59a29d8bfa687246e779b
Bijan Mousavi <mousavi.bijan@gmail.com>
2026-07-19 18:13:16 +0200

    chore(standards): pin unified Rust gate contract

--- makes/bijux-std.mk
+++ makes/bijux-std.mk
@@ -1,6 +1,6 @@
 BIJUX_STD_CHECK_SCRIPT ?= .bijux/shared/bijux-checks/check-bijux-std.sh
 BIJUX_STD_UPDATE_SCRIPT ?= .bijux/shared/bijux-checks/update-bijux-std.sh
-BIJUX_STD_REF ?= b038ca9848c62d4f5b2f617fc176d0b932d9c3b2
+BIJUX_STD_REF ?= f6e840158ecc2b75f589a7b3a66e670cc3e63773
 BIJUX_STD_GIT_URL ?= https://github.com/bijux/bijux-std.git
 BIJUX_STD_CAPABILITIES ?= docs python rust
 BIJUX_STD_UPDATE_CHANNEL ?= branch
